583e155772049a9892bf2df5e5b7cb4b9fea39f9
Luffy20241 <168181882+Luffy20241@users.noreply.github.com>
2024-06-21 22:02:51 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -18,14 +18,7 @@ from pyrogram import Client
 from bot.config import Config
 
 
-from aiohttp import web
-from .route import routes
 
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
 
 # dont think ne dumb (c) @Animes_Encoded 
 AUTH_USERS = set(Config.AUTH_USERS)
